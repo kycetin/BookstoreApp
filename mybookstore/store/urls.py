a8c1ec3e43c0325569dc5f96aaaccb3d31ea5ed0
Cetin Kaya <kayacetinn@gmail.com>
2021-05-08 17:41:02 +0300

    database init, User table added,search bar update

--- mybookstore/store/urls.py
+++ mybookstore/store/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home,),
+    path('', views.home,name="home"),
     path('search_bar/<int:page>/', views.store_search_bar),
     path('search/<str:search>/', views.store_search,name="store_search"),
     path('search/<str:search>/<int:page>/', views.store_search_page,name="store_search_page"),
