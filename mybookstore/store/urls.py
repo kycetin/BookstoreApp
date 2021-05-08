68545525342658b5c07c8710d1493581759e76eb
Cetin Kaya <kayacetinn@gmail.com>
2021-05-08 16:07:04 +0300

    book search and pagination added

--- mybookstore/store/urls.py
+++ mybookstore/store/urls.py
@@ -5,4 +5,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('search/<str:search>/', views.store_search),
+    path('search/<str:search>/<int:page>/', views.store_search_page,name="store_search_page"),
 ]
--- mybookstore/store/views.py
+++ mybookstore/store/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render
-
-# Create your views here.
-
 from django.http import HttpResponse,JsonResponse
 
 import requests
@@ -9,7 +6,35 @@ import json
 import math
 import pprint
 
+def create_range(total):
+    range_list = []
+    pag_size = math.ceil(total/10)
+    if pag_size > 100:
+        pag_size = 100
+    for i in range(1,pag_size+1):
+        if i > 100:
+            pag_size = 100
+            break
+        range_list.append(i)
+    return pag_size,range_list
+
 def home(request):
     response = requests.get(f"https://api.itbook.store/1.0/new")
     data = response.json()
-    return render(request,'index.html',{"ls":data["books"]})
+    total = int(data["total"])
+    pag_size,range_list = create_range(total)
+    return render(request,'index.html',{"ls":data["books"],"pg":pag_size,"range":range_list,"current_page":1 })
+
+def store_search(request,search):
+    response = requests.get(f"https://api.itbook.store/1.0/search/{search}")
+    data = response.json()
+    total = int(data["total"])
+    pag_size,range_list = create_range(total)
+    return render(request,'index.html',{"ls":data["books"],"pg":pag_size,"keyword":search,"range":range_list,"current_page":1 })
+
+def store_search_page(request,search,page):
+    response = requests.get(f"https://api.itbook.store/1.0/search/{search}/{page}")
+    data = response.json()
+    total = int(data["total"])
+    pag_size,range_list = create_range(total)
+    return render(request,'index.html',{"ls":data["books"],"pg":pag_size,"range":range_list,"keyword":search,"current_page":page })
