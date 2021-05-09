c622e92bb4493564d9d2a8793a280cdc7f4b7b72
Cetin Kaya <kayacetinn@gmail.com>
2021-05-09 23:34:51 +0300

    bookmarks page pagination bug fixed

--- mybookstore/store/views.py
+++ mybookstore/store/views.py
@@ -74,7 +74,6 @@ def add_bookmark(request):
     total = int(user.bookmarks['total'])
     user_books = user.bookmarks['books']
     if user.bookmarks['total'] == 0:
-            print("burda")
             user_books.append(book)
             user.bookmarks['books'] = user_books
             user.bookmarks['total'] += 1
@@ -83,7 +82,6 @@ def add_bookmark(request):
     else:
         for i in range(len(user_books)):
             if user_books[i]['isbn13'] == button_id:
-                print("exist")
                 return HttpResponse(json.dumps({"response":"exist","data":button_id}), content_type='application/json')
                 break
     user_books.append(book)
@@ -91,8 +89,6 @@ def add_bookmark(request):
     user.bookmarks['total'] += 1
     user.save()
     user = User.objects.get(username="cetin")
-    pprint.pprint(user.bookmarks)
-    print(user.bookmarks['total'])
     return HttpResponse(json.dumps({"response":"added","data":button_id}), content_type='application/json')
 
 def bookmarks(request):
@@ -100,8 +96,15 @@ def bookmarks(request):
     user = User.objects.get(username="cetin")
     books = user.bookmarks
     total = int(user.bookmarks['total'])
+    user_books = user.bookmarks['books']
     pag_size,range_list = create_range(total)
-    return render(request,'bookmark.html',{"ls":user.bookmarks['books'],"pg":pag_size,"range":range_list,"keyword":user,"current_page":1})
+    data = []
+    page=1
+    for i in range((page*10)-10,(page*10)):
+        if i == total:
+            break
+        data.append(user_books[i])
+    return render(request,'bookmark.html',{"ls":data,"pg":pag_size,"range":range_list,"keyword":user,"current_page":1})
 
 def bookmarks_page(request,page):
     
@@ -114,7 +117,6 @@ def bookmarks_page(request,page):
     for i in range((page*10)-10,(page*10)):
         if i == total:
             break
-        print(user_books[i])
         data.append(user_books[i])
     return render(request,'bookmark.html',{"ls":data,"pg":pag_size,"range":range_list,"keyword":user,"current_page":page})
 
@@ -122,11 +124,9 @@ def delete_bookmark(request):
     
     user = User.objects.get(username="cetin")
     button_id = request.POST['button_id']
-    print(button_id)
     page = int(request.POST['current'])
     total = int(user.bookmarks['total'])
     user_books = user.bookmarks['books']
-    print("book len",len(user_books))
     if len(user_books) == 0:
             del user_books[0]
             user.bookmarks['books'] = user_books
@@ -134,10 +134,7 @@ def delete_bookmark(request):
             user.save()
     for i in range(len(user_books)):
         if user_books[i]['isbn13'] == button_id:
-            print("deleted index",i)
-            print("try delete")
             del user_books[i]
-            print("deleted")
             user.bookmarks['books'] = user_books
             user.bookmarks['total'] -= 1
             user.save()
@@ -148,9 +145,7 @@ def delete_bookmark(request):
     pag_size = math.ceil(total/10)
     data = []
     pag_size,range_list = create_range(total)
-    print("total",total)
     for i in range((page*10)-10,(page*10)):
-        print(i)
         if i == total:
             break
         data.append(user_books[i])
@@ -159,7 +154,6 @@ def delete_bookmark(request):
 def read_more(request):
     
     button_id = request.POST['button_id']
-    print(button_id)
     response = requests.get(f"https://api.itbook.store/1.0/books/{button_id}")
     data = response.json()
     return HttpResponse(json.dumps({"data":data}), content_type='application/json')
